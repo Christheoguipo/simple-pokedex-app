772c8fd7d762ba27f99998d1019998b146d62873
Ctheo <85225231+Christheoguipo@users.noreply.github.com>
2023-08-05 15:59:51 +0200

    Update script.js
    - Remove previous codes.
    - Add 3 pokemons to pokemonList.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-alert('Hello World');
-
-let favoriteFood = "Chicken Stew";
-document.write(favoriteFood);
+let pokemonList = [
+  {name: 'Gengar', height: 1.5, types: ['Ghost', 'Poison']},
+  {name: 'Dragonite', height: 2.2, types: ['Dragon', 'Flying']},
+  {name: 'Mankey', height: 0.5, types: ['Fighting']}
+];
