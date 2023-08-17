94cd86c82a87534ac7a7e1168e494dc3064062b3
Ctheo <85225231+Christheoguipo@users.noreply.github.com>
2023-08-17 17:30:00 +0200

    Update scripts.js
    - Wrap pokemonList in IIFE which has 2 objects to show and add into the pokemonList.
    - Updated the 'forEach' loop to get the list from IIFE.

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,44 @@
-let pokemonList = [
-  {
-    name: 'Gengar',
-    height: 1.5,
-    types: ['Ghost', 'Poison']
-  },
-  {
-    name: 'Dragonite',
-    height: 2.2,
-    types: ['Dragon', 'Flying']
-  },
-  {
-    name: 'Mankey',
-    height: 0.5,
-    types: ['Fighting']
+
+// This IIFE wraps the pokemonList to avoid any accidental changes 
+let pokemonRepository = (function () {
+
+  let pokemonList = [
+    {
+      name: 'Gengar',
+      height: 1.5,
+      types: ['Ghost', 'Poison']
+    },
+    {
+      name: 'Dragonite',
+      height: 2.2,
+      types: ['Dragon', 'Flying']
+    },
+    {
+      name: 'Mankey',
+      height: 0.5,
+      types: ['Fighting']
+    }
+  ];
+
+  function add(item) {
+    pokemonList.push(item);
   }
-];
+
+  function getAll() {
+      return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  } 
+})();
 
 let bigPokemon = '';
 let bigPokemonClass = '';
 
-// This writes all the Pokemons in the list (their name and height)
-// for (i = 0; i < pokemonList.length; i++) {
-
-pokemonList.forEach(function(pokemon) {
+// This loops through and writes all the Pokemons from the repository to the list (their name and height)
+pokemonRepository.getAll().forEach(function (pokemon) {
 
   // This highlights big pokemons (height > 2.0)  
   if (pokemon.height > 2.0) {
@@ -36,14 +52,17 @@ pokemonList.forEach(function(pokemon) {
   bigPokemonClass = '';
 });
 
+
+
+
 function divide(dividend, divisor) {
-  if(divisor === 0) {
+  if (divisor === 0) {
     return 'You\'re trying to divide by zero.';
   } else {
     return dividend / divisor;
   }
 }
- 
+
 console.log(divide(4, 2));
 console.log(divide(7, 0));
 console.log(divide(1, 4));
